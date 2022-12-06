2b8b1b687005e8a4fba97919158fa813ede7309b
yvaishnavi0412 <yadamreddy.v@northeastern.edu>
2022-12-06 01:48:27 -0500

    Billing Organization

---
+++ src/business/organization/BillingOrganization.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.organization;
+
+import Business.Role.BillingAdminRole;
+import Business.Role.PatientRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vaishnaviyadamreddy
+ */
+public class BillingOrganization extends Organization {
+
+    double funds;
+    
+    public BillingOrganization() {
+        super(Organization.Type.BillingDepartment.getValue());
+    }
+    public BillingOrganization(String name){
+        super(name);
+    }
+
+    
+     @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new BillingAdminRole());
+        return roles;
+    }
+    
+    @Override
+    public Type getType() {
+        return Organization.Type.BillingDepartment;
+    } 
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+
+    
+    
+}
