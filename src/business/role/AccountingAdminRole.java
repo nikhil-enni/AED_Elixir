a7bf80b1a28237b379dc949780cdc0b4129dab1f
Amisha03032000 <gokhale.ami@northeastern.edu>
2022-12-08 14:52:36 -0500

    EcoSystem and Enterprise

---
+++ src/business/role/AccountingAdminRole.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.role;
+
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Network.Network;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import userinterface.AccountingUnitAdminArea.AccountingUnitAdminWorkAreaJPanel;
+import userinterface.AccountingUnitAdminArea.AccountingUnitAdminWorkAreaJPanel;
+
+/**
+ *
+ * @author ashleshagokhale
+ */
+public class AccountingAdminRole extends Role{
+    public JPanel createWorkArea(JPanel userProcessContainer, 
+          UserAccount account,Organization organization,Enterprise enterprise,Network network,EcoSystem business) {
+          return new AccountingUnitAdminWorkAreaJPanel(userProcessContainer, enterprise, business);
+    }
+    
+    
+    @Override
+    public String toString(){
+        return (RoleType.AccountingAdminRole.getValue());
+    }
+}
